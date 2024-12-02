d42ff0bf82cc3caf6845d2bce44efa8fb22c469d
IamHaydenNg <ngheiyue.dee@gmail.com>
2024-12-02 16:09:25 +0800

    Class 264: Storing the Picked Image Locally

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'package:favourite_places/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   // The state managed by riverpod must not be mutated. You must not edit it in memory.
@@ -9,8 +11,13 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
 
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title, File image, PlaceLocation location) {
-    final newPlace = Place(title: title, image: image, location: location);
+  void addPlace(String title, File image, PlaceLocation location) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$fileName');
+
+    final newPlace =
+        Place(title: title, image: copiedImage, location: location);
     // state provide by riverpod, means old state
     state = [newPlace, ...state];
   }
